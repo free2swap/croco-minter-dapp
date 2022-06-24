4cbb9e7e9df19ee1b0300a87886947b13288883a
xbank20 <dray2078@gmail.com>
2022-06-25 01:54:29 +0800

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x7C8C333AE913e47B5fa517E0A22CcE39fCEC7759"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
